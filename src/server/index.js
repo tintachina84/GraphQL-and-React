6f9958844fe93d26f6c2be1d75d19043757ee3c2
seonghyeon.seo <seonghyeon.seo@stonebuild.co.kr>
2023-07-31 15:22:15 +0900

    Added: GraphQL inits.

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,44 @@
 import express from 'express';
+import path from 'path';
+import helmet from 'helmet';
+import cors from 'cors';
+import compress from 'compression';
+import services from './services';
 
 const app = express();
+const root = path.join(__dirname, '../../');
+app.use(compress());
+if(process.env.NODE_ENV === 'production') {
+  app.use(helmet());
+  app.use(helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      imgSrc: ["'self'", "data:", "*.amazonaws.com"]
+    }
+  }));
+}
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+app.use(cors());
+app.use('/', express.static(path.join(root, 'dist/client')));
+app.use('/uploads', express.static(path.join(root, 'uploads')));
 
-app.get('*', (req, res) => {
-  res.send('Hello World!');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(root, '/dist/client/index.html'));
 });
-app.listen(8000, () => console.log('Server is running on localhost:8000'));
 
+const serviceNames = Object.keys(services);
+for (let i = 0; i < serviceNames.length; i += 1) {
+  const name = serviceNames[i];
+  if (name === 'graphql') {
+    (async () => {
+      await services[name].start();
+      services[name].applyMiddleware({ app });
+    })();
+  } else {
+    app.use('/${name}', services[name]);
+  }
+}
+
+app.listen(8000, () => console.log('Server is running on localhost:8000'));
